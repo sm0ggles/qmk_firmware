2b5ef3cf60f840c5eadbc004eb6bf6325fa741f3
sm0ggles <sm0g@protonmail.com>
2018-12-10 01:30:21 -0500

    wip xd60

--- keyboards/xd60/keymaps/sm0g/keymap.c
+++ keyboards/xd60/keymaps/sm0g/keymap.c
@@ -26,20 +26,20 @@ enum {
 };
 
   // _BL: Base Layer
-  LAYOUT_all(
-      KC_ESC,  TD(TD_F1),    TD(TD_F2),    TD(TD_F3),    TD(TD_F4),   TD(TD_F5),   TD(TD_F6),   TD(TD_F7),   TD(TD_F8),   KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSLS,  KC_GRV,    \
+[_BL] = LAYOUT_all(
+      KC_ESC,  TD(TD_F1),    TD(TD_F2),    TD(TD_F3),    TD(TD_F4),   TD(TD_F5),   TD(TD_F6),   TD(TD_F7),   TD(TD_F8),   TD(TD_F9),    TD(TD_F10),    TD(TD_F11),  TD(TD_F12),  KC_BSLS,  KC_GRV,    \
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,    KC_LBRC,  KC_RBRC,           KC_BSPC,   \
       CTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT,  KC_NO,             KC_ENT,    \
       KC_LSPO, KC_NO,   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH,  KC_NO, KC_RSPC,  MO(_FL),      \
       KC_CAPS, KC_LGUI, KC_LALT,                          KC_SPC,                          F(0),    KC_RALT,  KC_NO, KC_RGUI,    KC_RCTL),
 
-  // 1: Function Layer
-  LAYOUT_all(
-      KC_ESC,   KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,   KC_F6,   KC_F7,   KC_F8,  KC_F9,  KC_F10, KC_F11,   KC_F12,  KC_F13,   KC_F14,    \
-      KC_NO,   KC_NO,   KC_UP,   KC_END,  KC_NO,  KC_NO,   KC_CALC, KC_PGUP, KC_NO, KC_NO,    KC_NO,  KC_NO,    KC_NO,             KC_DEL,    \
-      KC_NO,   KC_LEFT, KC_DOWN, KC_RIGHT,KC_NO,  KC_NO,   KC_HOME, KC_PGDN, KC_NO,  KC_NO,   KC_NO,  KC_PGUP,  KC_NO,             KC_ENT,    \
-      KC_LSFT, KC_NO,   KC_NO,   KC_DEL,  BL_STEP,KC_NO,   KC_NO,   KC_VOLD, KC_VOLU,KC_MUTE, KC_NO,  KC_PGDN,  KC_RSFT, KC_NO,    KC_NO,      \
-      KC_LCTL, KC_LGUI, KC_LALT,                           KC_SPC,                            F(0),   KC_RALT,  KC_NO, KC_RGUI,    KC_RCTL),
+  // _FL: Function Layer
+[_FL] = LAYOUT_all(
+      _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,   KC_F6,   KC_F7,   KC_F8,  KC_F9,  KC_F10, KC_F11,   KC_F12,  KC_F13,   TG(_GL),    \
+      _______,   KC_NO,   KC_UP,   KC_END,  KC_NO,  KC_NO,   KC_CALC, KC_PGUP, KC_NO, KC_PGUP,    KC_UP,  KC_UPDN,    KC_NO,             KC_DEL,    \
+      KC_CAPS,   KC_LEFT, KC_DOWN, KC_RIGHT,KC_NO,  KC_NO,   KC_HOME, KC_PGDN, KC_NO,  KC_NO,   KC_LEFT,  KC_RIGHT,  KC_NO,             KC_ENT,    \
+      _______, KC_NO,   KC_NO,   KC_DEL,  BL_STEP,KC_NO,   KC_NO,   KC_VOLD, KC_VOLU,KC_MUTE, KC_NO,  KC_DOWN,  KC_RSFT, KC_NO,    _______,      \
+      _______, _______, _______,                           KC_SPC,                            F(0),   KC_RALT,  KC_NO, KC_RGUI,    KC_RCTL),
 
 };
 
