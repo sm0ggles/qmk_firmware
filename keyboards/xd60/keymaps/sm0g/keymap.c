237d978bfb9011d90c43385a466c4d2cb70a76bc
sm0ggles <sm0g@protonmail.com>
2018-12-10 20:53:39 -0500

    update

--- keyboards/xd60/keymaps/sm0g/keymap.c
+++ keyboards/xd60/keymaps/sm0g/keymap.c
@@ -7,23 +7,22 @@
 #define _GM 2
 #define _GF 3
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-// Tapdance declarations
 enum {
-	TD_F1 = 0,
-	TD_F2,
-	TD_F3,
-	TD_F4,
-	TD_F5,
-	TD_F6,
-	TD_F7,
-	TD_F8,
-	TD_F9,
-	TD_F10,
-	TD_F11,
-	TD_F12,
+  TD_F1 = 0,
+  TD_F2,
+  TD_F3,
+  TD_F4,
+  TD_F5,
+  TD_F6,
+  TD_F7,
+  TD_F8,
+  TD_F9,
+  TD_F10,
+  TD_F11,
+  TD_F12,
 };
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   // _BL: Base Layer
 [_BL] = LAYOUT_all(
@@ -35,11 +34,11 @@ enum {
 
   // _FL: Function Layer
 [_FL] = LAYOUT_all(
-      _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,   KC_F6,   KC_F7,   KC_F8,  KC_F9,  KC_F10, KC_F11,   KC_F12,  KC_F13,   TG(_GL),    \
-      _______,   KC_NO,   KC_UP,   KC_END,  KC_NO,  KC_NO,   KC_CALC, KC_PGUP, KC_NO, KC_PGUP,    KC_UP,  KC_UPDN,    KC_NO,             KC_DEL,    \
-      KC_CAPS,   KC_LEFT, KC_DOWN, KC_RIGHT,KC_NO,  KC_NO,   KC_HOME, KC_PGDN, KC_NO,  KC_NO,   KC_LEFT,  KC_RIGHT,  KC_NO,             KC_ENT,    \
+      _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,   KC_F6,   KC_F7,   KC_F8,  KC_F9,  KC_F10, KC_F11,   KC_F12,  KC_F13,   _______,    \
+      RGB_MOD,   RGB_VAI,   RGB_HUI,   RGB_VAI,  KC_NO,  KC_NO,   KC_CALC, KC_PGUP, KC_NO, KC_PGUP,    KC_UP,  KC_PGDN,    KC_NO,             KC_DEL,    \
+      KC_CAPS,   RGB_VAD, RGB_HUD, RGB_VAD,KC_NO,  KC_NO,   KC_HOME, KC_PGDN, KC_NO,  KC_NO,   KC_LEFT,  KC_RIGHT,  KC_NO,             KC_ENT,    \
       _______, KC_NO,   KC_NO,   KC_DEL,  BL_STEP,KC_NO,   KC_NO,   KC_VOLD, KC_VOLU,KC_MUTE, KC_NO,  KC_DOWN,  KC_RSFT, KC_NO,    _______,      \
-      _______, _______, _______,                           KC_SPC,                            F(0),   KC_RALT,  KC_NO, KC_RGUI,    KC_RCTL),
+      RGB_MODE_RAINBOW, _______, _______,                           KC_SPC,                            F(0),   KC_RALT,  KC_NO, KC_RGUI,    KC_RCTL),
 
 };
 
@@ -67,3 +66,18 @@ void matrix_scan_user(void) {
   // Empty
 };
 
+// Tapdance definitions. Tap Dance F Keys.
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [TD_F1] = ACTION_TAP_DANCE_DOUBLE(KC_1, KC_F1),
+  [TD_F2] = ACTION_TAP_DANCE_DOUBLE(KC_2, KC_F2),
+  [TD_F3] = ACTION_TAP_DANCE_DOUBLE(KC_3, KC_F3),
+  [TD_F4] = ACTION_TAP_DANCE_DOUBLE(KC_4, KC_F4),
+  [TD_F5] = ACTION_TAP_DANCE_DOUBLE(KC_5, KC_F5),
+  [TD_F6] = ACTION_TAP_DANCE_DOUBLE(KC_6, KC_F6),
+  [TD_F7] = ACTION_TAP_DANCE_DOUBLE(KC_7, KC_F7),
+  [TD_F8] = ACTION_TAP_DANCE_DOUBLE(KC_8, KC_F8),
+  [TD_F9] = ACTION_TAP_DANCE_DOUBLE(KC_9, KC_F9),
+  [TD_F10] = ACTION_TAP_DANCE_DOUBLE(KC_0, KC_F10),
+  [TD_F11] = ACTION_TAP_DANCE_DOUBLE(KC_MINS, KC_F11),
+  [TD_F12] = ACTION_TAP_DANCE_DOUBLE(KC_EQL, KC_F12),
+};
