5001a9876f280c6a3e596e5d14b73643bb7e7a7e
sm0ggles <chance.monn@mailbox.org>
2019-08-18 01:40:02 -0400

    updated koyu

--- keyboards/wilba_tech/rama_works_koyu/keymaps/sm0g/keymap.c
+++ keyboards/wilba_tech/rama_works_koyu/keymaps/sm0g/keymap.c
@@ -4,8 +4,7 @@
 # define _BL 0
 # define _FL 1
 # define _GM 2
-# define _GF 3
-# define _NU 4
+# define _NU 3
 
 // ESC to NUM
 # define ESC_NU LT(_NU, KC_ESC)
@@ -34,21 +33,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,          KC_DEL,
 		KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,                    KC_PGUP,
 		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                   KC_UP,   KC_PGDN,
-		KC_CAPS,   KC_NO, KC_LALT,                            KC_SPC,                                      MO(_GF),            KC_LEFT, KC_DOWN, KC_RGHT),
-
-
-	// Fn3 Layer
-	[_GF] = LAYOUT_all(
-		_______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,  TG(_GM),  KC_NLCK,
-		_______, EF_DEC, EF_INC, H1_DEC, H1_INC, H2_DEC, H2_INC, _______, _______, _______, _______, _______, BR_DEC, BR_INC,          _______,
-		RGB_TOG, _______, _______, S1_DEC, S1_INC, S2_DEC, S2_INC, _______, _______, _______, _______, ES_DEC, ES_INC,                   KC_HOME,
-		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______, KC_END,
-		_______, _______, _______,                            _______,                                     _______,          KC_MPRV, KC_MPLY, KC_MNXT),
+		KC_CAPS,   KC_NO, KC_LALT,                            KC_SPC,                                      MO(_FL),            KC_LEFT, KC_DOWN, KC_RGHT),
 
 
 	// NumPad Layer
 	[_NU] = LAYOUT_all(
-		XXXXXXX,  XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+		_______,  XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
 		XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_P7, KC_P8, KC_P9, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,
 		XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_P4, KC_P5, KC_P6, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX,
 		XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_P1, KC_P2, KC_P3, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX,
